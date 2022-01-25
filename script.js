85be2f61ca828eeaa1749bc03195197530cfb117
Chase Young <chasekyoung@gmail.com>
2022-01-24 19:30:05 -0500

    fixed progression

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 var current = 0;
 var timeLimit = 20;
 var test = [{
-   question: "What is a data type with two possible values of true or false?",
-   choices: ["Boolean", "Array", "String", "Function"]
-}
+        question: "What is a data type with two possible values of true or false?",
+        choices: ["Boolean", "Array", "String", "Function"]
+    },
+    {   
+        question: "Are Java and Javascript the same?",
+        choices: ["Yes", "No",]
+    }
 ]
 
 function displayChoices() {
@@ -21,3 +25,12 @@ function displayChoices() {
         )
     }
 }
+
+function startQuiz(){
+    document.querySelector("#container").classList.remove("hide")
+    document.querySelector(".start").classList.add("hide")
+}
+
+document.querySelector(".start").addEventListener("click", startQuiz)
+
+displayChoices()
