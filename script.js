be65a232c46b139505cb5ee5672694622594e5ed
Chase Young <chasekyoung@gmail.com>
2022-01-24 20:27:42 -0500

    questions now appear, added styling

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var current = 0;
 var timeLimit = 20;
+var questionText = document.querySelector("#multiQuestions");
 var test = [{
         question: "What is a data type with two possible values of true or false?",
         choices: ["Boolean", "Array", "String", "Function"]
@@ -7,10 +8,15 @@ var test = [{
     {   
         question: "Are Java and Javascript the same?",
         choices: ["Yes", "No",]
+    },
+    {
+        question: "A ______ is a block of code designed to perform a particular task.",
+        choices: ["String", "Boolean", "Function", "String"]
     }
 ]
 
 function displayChoices() {
+    questionText.textContent = test[current].question
     for (var i = 0; i < test[current].choices.length; i++) {
         var options = document.createElement("p")
         options.classList.add("options")
@@ -27,6 +33,7 @@ function displayChoices() {
 }
 
 function startQuiz(){
+    document.querySelector("#multiQuestions").classList.remove("hide")
     document.querySelector("#container").classList.remove("hide")
     document.querySelector(".start").classList.add("hide")
 }
