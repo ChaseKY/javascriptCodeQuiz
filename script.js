6a857fb8585b6a2662b2ee30de37b7040b640acc
Chase Young <chasekyoung@gmail.com>
2022-01-26 20:56:47 -0500

    fixed timer and added game over function

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+var Uscore = document.querySelector("#scoreValue");
+var highscore = 0;
 var current = 0;
+var timer = 20;
+var timerStart;
+var initialsInput = document.querySelector("#initials");
+var submitButton = document.querySelector("#submit");
+var display = document.querySelector("#time");
 var questionText = document.querySelector("#multiQuestions");
 var test = [{
         question: "What is a data type with two possible values of true or false?",
@@ -21,19 +28,21 @@ function startQuiz() {
     document.querySelector("#multiQuestions").classList.remove("hide")
     document.querySelector("#container").classList.remove("hide")
     document.querySelector(".start").classList.add("hide")
+
+    startTimer();
 }
 
-function startTimer(duration, display) {
-    var timer = duration, seconds;
-    setInterval(function() {
+function startTimer() {
+    timerStart = setInterval(function() {
         seconds = parseInt(timer % 60, 10);
         seconds = seconds < 10 ? "0" + seconds : seconds;
         display.textContent = seconds;
         if (--timer < 0) {
-            timer = duration;
+            gameOver()
         }
     },1000);
 }
+
 function displayChoices() {
     questionText.textContent = test[current].question
     for (var i = 0; i < test[current].choices.length; i++) {
@@ -41,28 +50,52 @@ function displayChoices() {
         options.classList.add("options")
         options.textContent = test[current].choices[i]
         document.querySelector("#container").appendChild(options)
-        options.addEventListener("click", function() {
+        options.addEventListener("click", function(e) {
+            correctAnswer(e.target.textContent)
+            
             current++;
             document.querySelector("#container").innerHTML = ""
-
+                if (current === test.length) {
+                    gameOver()
+                }
             displayChoices()
         }
         )
     }
 }
 
+function gameOver() {
+    document.querySelector("#multiQuestions").classList.add("hide")
+    document.querySelector("#container").classList.add("hide")
+    document.querySelector(".userInput").classList.remove("hide")
+    document.querySelector(".submitBtn").classList.remove("hide")
+    clearInterval(timerStart)
+    
+    
+}
+
 //Put function for correct answers
-//function correctAnswer() {
-//    if (options.textContent == correct) {
-//        document.querySelector(".page").
-//    }
-//}
+function correctAnswer(selectedChoice) {
+    if (selectedChoice == test[current].correct) {
+        // document.querySelector("#page").style.backgroundColor = "green";
+        highscore++;
+    }
+}
 
 document.querySelector(".start").addEventListener("click", startQuiz)
+document.querySelector(".userInput").classList.add("hide")
+document.querySelector(".submitBtn").classList.add("hide")
+//document.querySelector(".start").addEventListener("click", startTimer)
+//    var timeLimit = 20,
+//       display = document.querySelector('#time');
+//    startTimer(timeLimit, display);
+//submitButton.addEventListener("click", function(event) {
+//    event.preventDefault();
+//    var user = {
+//        initials: initialsInput.value,
+//        score: 
+//    }
+//    localStorage.setItem("user", JSON.stringify(user));
 
-document.querySelector(".start").addEventListener("click", startTimer)
-    var timeLimit = 20,
-        display = document.querySelector('#time');
-    startTimer(timeLimit, display);
-
+//});
 displayChoices()
